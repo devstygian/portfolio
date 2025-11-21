bb95ba14d8a7dd03ffeca674c2ae8770ec99a239
Stygian <hackstygian@gmail.com>
2025-11-21 17:12:53 +0800

    added hamburger-bar in navbar..Is it responsive now?

--- script.js
+++ script.js
@@ -17,10 +17,30 @@ function toggleProject(button) {
 }
 
 
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav');
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
 
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('action');
-});
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
 
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+
+// scrolling efect on navbar
+  window.addEventListener("scroll", function () {
+    const navbar = document.getElementById("navbar");
+    if (window.scrollY > 50) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
