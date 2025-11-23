bcb2e1534503e98b71912f59985835c49d9337d5
Stygian <hackstygian@gmail.com>
2025-11-23 14:51:57 +0800

    fixed screed size in project for smaller screen

--- script.js
+++ script.js
@@ -34,13 +34,3 @@ function closeMenu() {
     navMenu.classList.remove("active");
 }
 
-
-// scrolling efect on navbar
-  window.addEventListener("scroll", function () {
-    const navbar = document.getElementById("navbar");
-    if (window.scrollY > 50) {
-      navbar.classList.add("scrolled");
-    } else {
-      navbar.classList.remove("scrolled");
-    }
-  });
